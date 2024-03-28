c66d00e0e2b210af71573a9bc11c9923861159f5
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-03-28 20:28:13 +0530

    finalised client and server

--- Readme.md
+++ Readme.md
@@ -16,3 +16,39 @@ protoc --go_out=paths=source_relative:. \
        types/ptypes.proto
 
 ```
+
+# How to run?
+
+1. Start the connectRPC server
+```
+cd server/
+go run server.go
+```
+
+2. Test the API's using curl command
+
+- Test the User service
+```
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d '{"user_id": "123"}' \
+     http://localhost:8080/user
+
+```
+
+- Test the Tweets service
+
+```
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d '{"hashtag": "#golang"}' \
+     http://localhost:8080/tweets
+
+```
+
+3. You can also use the client to test the server
+
+```
+cd client/
+go run client.go
+```
--- client/client.go
+++ client/client.go
@@ -1,8 +1,33 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	pb "github.com/adarsh-jaiss/grpc-assingment/types"
+	"google.golang.org/grpc"
 )
 
 func main() {
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTwitterServiceClient(conn)
+	// Call GetUser RPC
+	userResponse, err := client.GetUser(context.Background(), &pb.UserRequest{UserId: "1"})
+	if err != nil {
+		log.Fatalf("Error calling GetUser: %v", err)
+	}
+	fmt.Println("User Response:", userResponse)
+
+	// Call GetTweets RPC
+	tweetsResponse, err := client.GetTweets(context.Background(), &pb.TweetsRequest{Hashtag: "internship"})
+	if err != nil {
+		log.Fatalf("Error calling GetTweets: %v", err)
+	}
+	fmt.Println("Tweets Response:", tweetsResponse)
 
 }
